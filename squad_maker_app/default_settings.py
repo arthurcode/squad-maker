5aac129ffd1e81ada816e412ea1a940397e3c106
Rhyan Arthur <rhyan.arthur@gmail.com>
2018-06-28 10:22:28 -0700

    web: use the json-generator.com REST API as our default data source
    This will make it easier to switch to the real REST API when it's
    available.

--- squad_maker_app/default_settings.py
+++ squad_maker_app/default_settings.py
@@ -3,7 +3,7 @@
 """ Contains default application settings. """
 
 from os.path import dirname, abspath, join
-from squad_maker_app.data_sources import get_file_data_source
+from squad_maker_app.data_sources import get_rest_data_source
 
 # useful constants
 APP_ROOT = dirname(abspath(__file__))
@@ -12,6 +12,6 @@ APP_STATIC = join(APP_ROOT, 'static')
 # IMPORTANT: The production config file MUST override SECRET_KEY with a key that is actually secret (and unique)
 SECRET_KEY = 'dev'
 
-# By default read player data from a local file. Once the REST API is available this can
-# be swapped out for a REST data source.
-PLAYER_SOURCE = get_file_data_source(join(APP_STATIC, 'players.json'))
+# By default read generated player data from json-generator.com.
+# TODO: When the player REST API is available update this uri.
+PLAYER_SOURCE = get_rest_data_source("http://www.json-generator.com/api/json/get/bVlwKzZWbm?indent=2")
